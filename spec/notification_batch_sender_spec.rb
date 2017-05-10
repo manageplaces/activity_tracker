23d1b831798b948db732022c9a35c3fe08098962
Alexander Lazarov <alexander.lazaroff@gmail.com>
2017-05-10 17:57:36 +0300

    Clean up specs

--- spec/notification_batch_sender_spec.rb
+++ spec/notification_batch_sender_spec.rb
@@ -34,12 +34,14 @@ describe ActivityTracker::NotificationBatchSender do
 
   specify 'when initialised with no params' do
     expect do
-      ns = ActivityTracker::NotificationBatchSender.new
+      ActivityTracker::NotificationBatchSender.new
     end.to raise_error(ArgumentError)
   end
 
   specify 'when notification is not closed' do
-    ns = ActivityTracker::NotificationBatchSender.new(notification_batch_amendable)
+    ns = ActivityTracker::NotificationBatchSender.new(
+      notification_batch_amendable
+    )
 
     expect(ns.process).to eq(false)
     expect(probe.count).to eq(0)
@@ -59,11 +61,16 @@ describe ActivityTracker::NotificationBatchSender do
   end
 
   specify 'when initialised with a single notification' do
-    ns = ActivityTracker::NotificationBatchSender.new(notification_batch_single_notification)
+    ns = ActivityTracker::NotificationBatchSender.new(
+      notification_batch_single_notification
+    )
 
     expect(ns.process).to eq(true)
     expect(probe.count).to eq(1)
-    expect(probe.first).to eq([notification_batch_single_notification.receiver, notification_batch_single_notification.notifications.to_a])
+    expect(probe.first).to eq(
+      [notification_batch_single_notification.receiver,
+       notification_batch_single_notification.activities]
+    )
 
     notification_batch_single_notification.reload
     expect(notification_batch_single_notification.is_sent).to eq(true)
