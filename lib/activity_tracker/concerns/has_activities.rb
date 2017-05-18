681abb87f7d5f654ec5ca19a831f69b3d235a8f2
Alexander Lazarov <alexander.lazaroff@gmail.com>
2017-05-18 19:18:03 +0300

    Add scopes

--- lib/activity_tracker/concerns/has_activities.rb
+++ lib/activity_tracker/concerns/has_activities.rb
@@ -5,7 +5,8 @@ module ActivityTracker
     extend ActiveSupport::Concern
 
     included do
-      has_many :activities, as: :scope, dependent: :destroy
+      has_many :scope_activities, as: :resource, dependent: :destroy
+      has_many :resource_activities, as: :resource, dependent: :destroy
 
       def track_activity(receivers, type, options = {})
         if self.is_a?(ActiveRecord::Base) && !options.include?(:scope)
